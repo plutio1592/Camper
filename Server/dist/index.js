ac9dfd9f3f637d37140935bddd551e2d0cc092ff
ldg <code920309@gmail.com>
2022-07-15 11:44:23 +0900

    ts파일 js컴파일

--- Server/dist/index.js
+++ Server/dist/index.js
@@ -3,11 +3,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+require("dotenv/config");
 const express_1 = __importDefault(require("express"));
 const router_1 = __importDefault(require("./router/router"));
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+// app.use(express.urlencoded({ extended: false }))
 app.get('/', (request, response, next) => {
-    response.send('정배홍이!! ts 쉽지않네!');
+    response.json(`정배홍이 되나?`);
 });
 app.use("/router", router_1.default);
 app.listen(4002, () => {
