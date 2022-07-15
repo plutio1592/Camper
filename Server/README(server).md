e6ec5af3cfcd803cc2126601073ca76adf2812a7
ldg <code920309@gmail.com>
2022-07-15 11:28:19 +0900

    배포테스트를 위한 axios/express 추가

---
+++ Server/README(server).md
@@ -0,0 +1,7 @@
+# Camper
+camper
+
+
+npm start: 컴파일해서 나온 app.js 파일을 돌릴 명령어
+npm dev : nodeon으로 타입스크립트 파일을 돌려볼 명령어(컴파일은 안되고 돌려볼 수만 있음)
+npm build : 실제로 컴파일해서 자바스크립트 파일 생성
--- Server/src/index.ts
+++ Server/src/index.ts
@@ -1,1 +1,21 @@
-console.log('정배홍이 준비됬나?????????????????')
+import 'dotenv/config'
+import express, {Request, Response, NextFunction} from 'express';
+import router from "./router/router";
+import cors from 'cors'
+
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+// app.use(express.urlencoded({ extended: false }))
+
+app.get('/', (request:Request, response:Response, next: NextFunction) => {
+  response.json(`정배홍이 되나?`);
+});
+
+app.use("/router", router);
+
+app.listen(4002,()=>{
+  console.log('start')
+})
